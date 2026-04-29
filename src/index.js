e452d336c5ffedea6902857a5ec11ddb3a4d6e0a
Michael Baron <barondaniel2001@gmail.com>
2026-04-28 22:41:50 -0500

    registro de rutas de auth, users y destinos en index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const connectDB = require('./config/database');
+const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
+const destinoRoutes = require('./routes/destino.routes');
 
 const app = express();
 
@@ -10,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/destinos', destinoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
